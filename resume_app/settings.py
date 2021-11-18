3f33fa5d4a07546825ef3eafa7edde41477880d3
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-18 16:05:08 +0530

    febkgkbrjw

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -152,6 +152,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
     
 django_heroku.settings(locals())
