d7e8abaa07e8245a02feab1d018d0ae40df667e0
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-18 22:56:21 +0530

    cv b

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include('main.urls', namespace="main")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
