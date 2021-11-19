a44b0e2e2f39112f1dbd25800c8b107e2072a6c5
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-19 23:33:14 +0530

    d

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ['paradoxportfolio.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
