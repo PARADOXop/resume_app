19555adeb735e73950b230407023184f33a984b3
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-19 23:55:22 +0530

    x

--- resume_app/urls.py
+++ resume_app/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace="main")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 if settings.DEBUG:
