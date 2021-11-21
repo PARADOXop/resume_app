5c04d3fcf991e5fc8da95ac13f7927179bff8b8f
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-21 23:12:48 +0530

    d

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) 
 
