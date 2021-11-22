d6efab06767037418f977aad97d18104fa9015f9
LEARNING PARADOX <ravirajkukade11@gmail.com>
2021-11-22 11:21:13 +0530

    Signed-off-by: LEARNING PARADOX <ravirajkukade11@gmail.com>sv

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) 
+ALLOWED_HOSTS=['localhost', 'paradoxportfolio.herokuapp.com']
 
 
 # Application definition
